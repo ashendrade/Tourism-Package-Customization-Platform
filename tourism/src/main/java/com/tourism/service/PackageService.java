7e99540a559ea2d66cb0c564f24e09bf998adcfc
IT25101436 <it25101436@my.sliit.lk>
2026-04-24 10:32:05 +0530

    Update PackageService.java

--- tourism/src/main/java/com/tourism/service/PackageService.java
+++ tourism/src/main/java/com/tourism/service/PackageService.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+Service
 public class PackageService {
 
     private static final String FILE_PATH = "packages.txt";
