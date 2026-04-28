4d7a547140dd1dd5072281408b9fc44705ccece1
IT25101436 <it25101436@my.sliit.lk>
2026-04-28 23:45:18 +0530

    Update PackageController.java

--- tourism/src/main/java/com/tourism/controller/PackageController.java
+++ tourism/src/main/java/com/tourism/controller/PackageController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/packages")
-public class PackageController {
+publi class PackageController {
 
     private final PackageService service;
 
