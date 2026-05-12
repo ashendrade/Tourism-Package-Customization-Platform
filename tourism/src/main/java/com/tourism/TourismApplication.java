0573b5f0c9dfd08eec7ee2adf3150a83849a4587
IT25101436 <it25101436@my.sliit.lk>
2026-05-12 11:02:18 +0530

    Update TourismApplication.java

--- tourism/src/main/java/com/tourism/TourismApplication.java
+++ tourism/src/main/java/com/tourism/TourismApplication.java
@@ -1,6 +1,7 @@
 package com.tourism;
 
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TourismApplication {
